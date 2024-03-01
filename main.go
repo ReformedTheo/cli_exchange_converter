9343a3ccf7281528b2ce92ccb39117a3176465e2
ReformedTheo <lopes.theo22@gmail.com>
2024-02-29 21:56:48 -0300

    included PAIR module

--- main.go
+++ main.go
@@ -1,31 +1,111 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
 )
 
-var API_KEY string = ""
-
-func selectCurrencies() {
-	fmt.Print("")
+type PAIR struct {
+	LastUpadate    string  `json:"time_last_update_utc"`
+	ConversionRate float64 `json:"conversion_rate"`
 }
 
-func main() {
-	fmt.Println(" *****Welcome to the CLI EXCHANGE CONVERTER***** \n\n\n Please input your https://www.exchangerate-api.com/  API KEY \n If you don't have one yet, you'll have to get one to use it")
+func pairCurrency(args []string) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Could not load .env file", err)
+	}
+	API_KEY, exists := os.LookupEnv("API_KEY")
+	if !exists {
+		log.Fatal("API KEY NOT FOUND")
+	}
+	var pair PAIR
+	baseCurrency := args[0]
+	targetCurrency := args[1]
+	url := "https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/" + baseCurrency + "/" + targetCurrency
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal("Could not access URL, check your API KEY", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println(url)
+		fmt.Println("Request failed with status code:", resp.StatusCode)
+		return
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+	err = json.Unmarshal(body, &pair)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		return
+	}
+	fmt.Printf("The conversion rate from %s to %s is %f to 1, last update at: %s.\n", baseCurrency, targetCurrency, pair.ConversionRate, pair.LastUpadate)
+
+}
+
+func error(arg string) {
+	log.Fatal("WRONG ARGUMENTS, CHECK HELP " + arg + ".")
+}
+
+func helpMe() {
+	fmt.Print("HELP ME")
+}
+
+func registerApiKey() {
+	var API_KEY string
 	fmt.Print("API KEY: ")
 	fmt.Scanln(&API_KEY)
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+	file, err := os.OpenFile(".env", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		log.Fatal("Could not open .env file ", err)
+	}
+	defer file.Close()
 
-	for {
-		var option int
-		fmt.Print("****MENU****\n\n 1 - Compare Currencies \n 0 - Quit app")
-
-		switch option {
-		case 1:
-			selectCurrencies()
-		case 0:
-			os.Exit(0)
+	// Check if API_KEY already exists
+	scanner := bufio.NewScanner(file)
+	var hasApiKey bool
+	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), "API_KEY=") {
+			hasApiKey = true
+			break
 		}
+	}
 
+	// If API_KEY doesn't exist, append it to the file
+	if !hasApiKey {
+		_, err = file.WriteString(fmt.Sprintf("API_KEY=%s\n", API_KEY))
+		if err != nil {
+			log.Fatal("Could not open .env file ", err)
+		}
+	}
+}
+func main() {
+	args := os.Args[1:]
+	switch args[0] {
+	case "help":
+		helpMe()
+	case "register":
+		registerApiKey()
+	case "pair":
+		if len(args) < 3 {
+			error(args[0])
+		}
+		pairCurrency(args[1:])
 	}
 }
